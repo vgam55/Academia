7b7652bac916b38ed35fa1e4a1c85d6925e48188
unknown <vgam55@gmail.com>
2020-02-07 17:28:49 +0100

    07/02/2020

--- resources/views/grupos/mostrar-grupos.blade.php
+++ resources/views/grupos/mostrar-grupos.blade.php
@@ -7,12 +7,18 @@
 		<a href="#" class="btn btn-primary"><i class='fas fa-plus'></i>  Crear Grupo</a> </h2>
 	</div>
 	
-
+   <!-- -->
+   
 	<!-- Generamos una tabla para visualizar los grupos y poder borrar o modificarlos -->
 	<div>
 	
    @include('grupos.crear-grupo')
-
+   
+   <form style="display:inline">
+   	<label for="filtrarGrupos">Filtrar Grupos</label>
+   	<input type="text" id="filtrarGrupos"></input>
+   	<button>Filtrar</button>
+   </form>
 
 	<table class="table">
 	  <thead class="thead-dark">
@@ -27,7 +33,17 @@
      		<tr>
 			      <th scope="row">{{$grupo->id}}</th>
 			      <td>{{$grupo->nombre}}</td>
-			      <td><img src=" {{ asset('borrar.png') }} "></td>			      
+			      <td>
+			      	<span><i class='fa fa-eye btn btn-success rounded text-center' title='Más información' ></i></span> 
+			      	<span><i class='fas fa-pencil-alt btn btn-info rounded' title='Editar'></i></span>
+			      	<form action="#" method="post" data-extra="#" style="display:inline"> 
+						<button class="btn btn-danger" style="padding: 0; height: 30px; width: 30px; margin: 5px 2px;">
+							 @csrf
+                             @method('DELETE')
+							<i class="far fa-trash-alt" title='Borrar registro'></i>
+						</button>
+			      	</form>
+			      </td>			      
 	    	</tr>		 
 		</tbody>
 	  @endforeach
